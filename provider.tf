08e8e73a5f8d06a86756dc6a33da4daac4cac0ea
vaish173 <vaishnavi.varadaraj999@gmail.com>
2026-07-15 13:06:54 +0530

    fix: embed subscription_id variable block directly inside provider declaration

--- provider.tf
+++ provider.tf
@@ -1,13 +1,18 @@
+variable "subscription_id" {
+  type        = string
+  description = "The Azure Subscription ID used by the provider"
+}
+
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 3.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
+  }
 }
 
 provider "azurerm" {
-    features {}
-    subscription_id = var.subscription_id
+  features {}
+  subscription_id = var.subscription_id
 }
